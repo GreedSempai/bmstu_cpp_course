a9307a983ed73494bc161c429813abe5c8e72ce1
GreedSempai <125344546+GreedSempai@users.noreply.github.com>
2026-04-08 14:54:44 +0300

    Update deepseek_cpp_20260408_b636cc.cpp

--- deepseek_cpp_20260408_b636cc.cpp
+++ deepseek_cpp_20260408_b636cc.cpp
@@ -8,174 +8,201 @@ namespace bmstu
 {
 struct nullopt_t
 {
-	constexpr explicit nullopt_t(int) {}
+    constexpr explicit nullopt_t(int) {}
 };
 inline constexpr nullopt_t nullopt{0};
 
 class bad_optional_access : public std::exception
 {
-   public:
-	const char* what() const noexcept override { return "Bad optional access"; }
+public:
+    const char* what() const noexcept override { return "Bad optional access"; }
 };
 
 template <typename T>
 class optional
 {
-   public:
-	optional() noexcept : is_initialized_(false) {}
+public:
+    // Default constructor
+    optional() noexcept : is_initialized_(false) {}
 
-	optional(nullopt_t) noexcept : is_initialized_(false) {}
+    // Nullopt constructor
+    optional(nullopt_t) noexcept : is_initialized_(false) {}
 
-	optional(const T& value) : is_initialized_(true) {
-		new (&data_[0]) T(value);
-	}
+    // Copy value constructor
+    optional(const T& value) : is_initialized_(true) {
+        new (&data_[0]) T(value);
+    }
 
-	optional(T&& value) : is_initialized_(true) {
-		new (&data_[0]) T(std::move(value));
-	}
+    // Move value constructor
+    optional(T&& value) : is_initialized_(true) {
+        new (&data_[0]) T(std::move(value));
+    }
 
-	optional(const optional& other) : is_initialized_(other.is_initialized_) {
-		if (is_initialized_) {
-			new (&data_[0]) T(other.value());
-		}
-	}
+    // Copy constructor
+    optional(const optional& other) : is_initialized_(other.is_initialized_) {
+        if (is_initialized_) {
+            new (&data_[0]) T(other.value());
+        }
+    }
 
-	optional(optional&& other) noexcept : is_initialized_(other.is_initialized_) {
-		if (is_initialized_) {
-			new (&data_[0]) T(std::move(other.value()));
-			other.is_initialized_ = false;
-		}
-	}
+    // Move constructor
+    optional(optional&& other) noexcept : is_initialized_(other.is_initialized_) {
+        if (is_initialized_) {
+            new (&data_[0]) T(std::move(other.value()));
+            other.reset();
+        }
+    }
 
-	~optional() {
-		reset();
-	}
+    // Destructor
+    ~optional() {
+        reset();
+    }
 
-	optional& operator=(const T& value) {
-		if (is_initialized_) {
-			this->value() = value;
-		} else {
-			new (&data_[0]) T(value);
-			is_initialized_ = true;
-		}
-		return *this;
-	}
+    // Assignment from value (copy)
+    optional& operator=(const T& value) {
+        if (is_initialized_) {
+            value() = value;
+        } else {
+            new (&data_[0]) T(value);
+            is_initialized_ = true;
+        }
+        return *this;
+    }
 
-	optional& operator=(T&& value) {
-		if (is_initialized_) {
-			this->value() = std::move(value);
-		} else {
-			new (&data_[0]) T(std::move(value));
-			is_initialized_ = true;
-		}
-		return *this;
-	}
+    // Assignment from value (move)
+    optional& operator=(T&& value) {
+        if (is_initialized_) {
+            value() = std::move(value);
+        } else {
+            new (&data_[0]) T(std::move(value));
+            is_initialized_ = true;
+        }
+        return *this;
+    }
 
-	optional& operator=(const optional& other) {
-		if (this != &other) {
-			reset();
-			if (other.is_initialized_) {
-				new (&data_[0]) T(other.value());
-				is_initialized_ = true;
-			}
-		}
-		return *this;
-	}
+    // Copy assignment
+    optional& operator=(const optional& other) {
+        if (this != &other) {
+            if (is_initialized_ && other.is_initialized_) {
+                value() = other.value();
+            } else if (is_initialized_) {
+                reset();
+            } else if (other.is_initialized_) {
+                new (&data_[0]) T(other.value());
+                is_initialized_ = true;
+            }
+        }
+        return *this;
+    }
 
-	// ИСПРАВЛЕННЫЙ move assignment
-	optional& operator=(optional&& other) noexcept {
-		if (this != &other) {
-			reset();
-			if (other.is_initialized_) {
-				new (&data_[0]) T(std::move(other.value()));
-				is_initialized_ = true;
-				other.reset();
-			}
-		}
-		return *this;
-	}
+    // Move assignment
+    optional& operator=(optional&& other) noexcept {
+        if (this != &other) {
+            if (is_initialized_ && other.is_initialized_) {
+                value() = std::move(other.value());
+                other.reset();
+            } else if (is_initialized_) {
+                reset();
+            } else if (other.is_initialized_) {
+                new (&data_[0]) T(std::move(other.value()));
+                is_initialized_ = true;
+                other.reset();
+            }
+        }
+        return *this;
+    }
 
-	optional& operator=(nullopt_t) noexcept {
-		reset();
-		return *this;
-	}
+    // Assignment from nullopt
+    optional& operator=(nullopt_t) noexcept {
+        reset();
+        return *this;
+    }
 
-	template<typename U, typename = std::enable_if_t<std::is_constructible_v<T, U>>>
-	optional& operator=(U&& value) {
-		if (is_initialized_) {
-			this->value() = std::forward<U>(value);
-		} else {
-			new (&data_[0]) T(std::forward<U>(value));
-			is_initialized_ = true;
-		}
-		return *this;
-	}
+    // Template assignment for convertible types (e.g., const char* to string)
+    template<typename U, typename = std::enable_if_t<std::is_constructible_v<T, U>>>
+    optional& operator=(U&& value) {
+        if (is_initialized_) {
+            value() = std::forward<U>(value);
+        } else {
+            new (&data_[0]) T(std::forward<U>(value));
+            is_initialized_ = true;
+        }
+        return *this;
+    }
 
-	T& operator*() & {
-		return value();
-	}
+    // Observers
+    bool has_value() const noexcept {
+        return is_initialized_;
+    }
 
-	const T& operator*() const& {
-		return value();
-	}
+    explicit operator bool() const noexcept {
+        return has_value();
+    }
 
-	T* operator->() {
-		return &value();
-	}
+    T& value() & {
+        if (!is_initialized_) {
+            throw bad_optional_access();
+        }
+        return *reinterpret_cast<T*>(&data_[0]);
+    }
 
-	const T* operator->() const {
-		return &value();
-	}
+    const T& value() const& {
+        if (!is_initialized_) {
+            throw bad_optional_access();
+        }
+        return *reinterpret_cast<const T*>(&data_[0]);
+    }
 
-	T&& operator*() && {
-		return std::move(value());
-	}
+    T&& value() && {
+        if (!is_initialized_) {
+            throw bad_optional_access();
+        }
+        return std::move(*reinterpret_cast<T*>(&data_[0]));
+    }
 
-	T& value() & {
-		if (!is_initialized_) {
-			throw bad_optional_access();
-		}
-		return *reinterpret_cast<T*>(&data_[0]);
-	}
+    // Dereference operators
+    T& operator*() & {
+        return value();
+    }
 
-	const T& value() const& {
-		if (!is_initialized_) {
-			throw bad_optional_access();
-		}
-		return *reinterpret_cast<const T*>(&data_[0]);
-	}
+    const T& operator*() const& {
+        return value();
+    }
 
-	T&& value() && {
-		if (!is_initialized_) {
-			throw bad_optional_access();
-		}
-		return std::move(*reinterpret_cast<T*>(&data_[0]));
-	}
+    T&& operator*() && {
+        return std::move(value());
+    }
 
-	template <typename... Args>
-	void emplace(Args&&... args) {
-		reset();
-		new (&data_[0]) T(std::forward<Args>(args)...);
-		is_initialized_ = true;
-	}
+    T* operator->() {
+        return &value();
+    }
 
-	void reset() noexcept {
-		if (is_initialized_) {
-			value().~T();
-			is_initialized_ = false;
-		}
-	}
+    const T* operator->() const {
+        return &value();
+    }
 
-	bool has_value() const noexcept {
-		return is_initialized_;
-	}
+    // Modifiers
+    void reset() noexcept {
+        if (is_initialized_) {
+            value().~T();
+            is_initialized_ = false;
+        }
+    }
 
-	explicit operator bool() const noexcept {
-		return has_value();
-	}
+    template <typename... Args>
+    void emplace(Args&&... args) {
+        reset();
+        new (&data_[0]) T(std::forward<Args>(args)...);
+        is_initialized_ = true;
+    }
 
-   private:
-	alignas(T) uint8_t data_[sizeof(T)];
-	bool is_initialized_ = false;
+private:
+    alignas(T) uint8_t data_[sizeof(T)];
+    bool is_initialized_ = false;
 };
-}  // namespace bmstu
+
+// Deduction guide
+template<typename T>
+optional(T) -> optional<T>;
+
+} // namespace bmstu
