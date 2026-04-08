97a41225acf3bc8eebc4743291c2879ae015b84e
GreedSempai <125344546+GreedSempai@users.noreply.github.com>
2026-04-08 16:39:41 +0300

    Update deepseek_cpp_20260408_b636cc.cpp

--- deepseek_cpp_20260408_b636cc.cpp
+++ deepseek_cpp_20260408_b636cc.cpp
@@ -1,25 +1,88 @@
- *  Executing task: C/C++: g++ build active file 
+// ==================== Iterator ====================
+struct iterator
+{
+    tree_node<K, V>* current_;
+    std::stack<tree_node<K, V>*> stack_;
+    mutable value_type pair_cache_;
 
-Starting build...
-/usr/bin/g++ -fdiagnostics-color=always -g /home/greed/yap/bmstu_cpp_course/tasks/bmstu_map/task_map/bmstu_map_test.cpp -o /home/greed/yap/bmstu_cpp_course/tasks/bmstu_map/task_map/bmstu_map_test
-In file included from /usr/include/gtest/gtest-printers.h:122,
-                 from /usr/include/gtest/gtest-matchers.h:49,
-                 from /usr/include/gtest/internal/gtest-death-test-internal.h:47,
-                 from /usr/include/gtest/gtest-death-test.h:43,
-                 from /usr/include/gtest/gtest.h:65,
-                 from /home/greed/yap/bmstu_cpp_course/tasks/bmstu_map/task_map/bmstu_map_test.cpp:3:
-/home/greed/yap/bmstu_cpp_course/tasks/bmstu_map/task_map/bmstu_map_test.cpp: In member function ‘virtual void MapTest_IteratorArrowOperator_Test::TestBody()’:
-/home/greed/yap/bmstu_cpp_course/tasks/bmstu_map/task_map/bmstu_map_test.cpp:327:31: error: base operand of ‘->’ has non-pointer type ‘bmstu::map<int, std::__cxx11::basic_string<char> >::iterator’
-  327 |                 EXPECT_TRUE(it->first == 1 || it->first == 2);
-      |                               ^~
-/home/greed/yap/bmstu_cpp_course/tasks/bmstu_map/task_map/bmstu_map_test.cpp:327:49: error: base operand of ‘->’ has non-pointer type ‘bmstu::map<int, std::__cxx11::basic_string<char> >::iterator’
-  327 |                 EXPECT_TRUE(it->first == 1 || it->first == 2);
-      |                                                 ^~
-/home/greed/yap/bmstu_cpp_course/tasks/bmstu_map/task_map/bmstu_map_test.cpp:328:32: error: base operand of ‘->’ has non-pointer type ‘bmstu::map<int, std::__cxx11::basic_string<char> >::iterator’
-  328 |                 EXPECT_FALSE(it->second.empty());
-      |                                ^~
+    iterator() : current_(nullptr), pair_cache_(K{}, V{}) {}
 
-Build finished with error(s).
+    explicit iterator(tree_node<K, V>* root, bool is_end = false)
+        : current_(nullptr), pair_cache_(K{}, V{})
+    {
+        if (is_end || root == nullptr)
+        {
+            return;
+        }
 
- *  The terminal process terminated with exit code: -1. 
- *  Terminal will be reused by tasks, press any key to close it. 
+        tree_node<K, V>* node = root;
+        while (node != nullptr)
+        {
+            stack_.push(node);
+            node = node->left;
+        }
+
+        if (!stack_.empty())
+        {
+            current_ = stack_.top();
+            stack_.pop();
+
+            tree_node<K, V>* rightNode = current_->right;
+            while (rightNode != nullptr)
+            {
+                stack_.push(rightNode);
+                rightNode = rightNode->left;
+            }
+        }
+    }
+
+    value_type operator*() const
+    {
+        return value_type(current_->key, current_->value);
+    }
+
+    value_type* operator->() const
+    {
+        // Обновляем кэш и возвращаем указатель на него
+        new (&pair_cache_) value_type(current_->key, current_->value);
+        return &pair_cache_;
+    }
+
+    iterator& operator++()
+    {
+        if (!stack_.empty())
+        {
+            current_ = stack_.top();
+            stack_.pop();
+
+            tree_node<K, V>* rightNode = current_->right;
+            while (rightNode != nullptr)
+            {
+                stack_.push(rightNode);
+                rightNode = rightNode->left;
+            }
+        }
+        else
+        {
+            current_ = nullptr;
+        }
+        return *this;
+    }
+
+    iterator operator++(int)
+    {
+        iterator temp = *this;
+        ++(*this);
+        return temp;
+    }
+
+    bool operator==(const iterator& other) const
+    {
+        return current_ == other.current_;
+    }
+
+    bool operator!=(const iterator& other) const
+    {
+        return current_ != other.current_;
+    }
+};
